29cce85218b4c06ec894ccab68ca0db8e9d69ce6
hiroyuki yamamoto <h.yama2396@gmail.com>
2023-09-09 18:35:25 +0900

    Add files via upload

--- R/ssea_ora.R
+++ R/ssea_ora.R
@@ -1,19 +1,11 @@
-ssea_ora <- function(detected, M){
-
-  ## SSEA
-  ALL <- detected # detected metabolites
-  umet <- unique(as.character(unlist(M))) # all metabolites in pathways
-
-  # ORA for each samples
+ssea_ora <- function (det_list, det_all, M) 
+{
   Z <- NULL
-  for(i in 1:nrow(X)){
-    index <- which(X[i,]>0) # threshold = 0 (detected metabolites)
-
-    B <- msea_ora(ALL[index], umet, M)
-    z <- -log(B$`Result of MSEA(ORA)`[,1])
-
-    Z <- rbind(Z,z)
+  for (i in 1:length(det_list)) {
+    B <- msea_ora(det_list[[i]], det_all, M)
+    z <- -log(B$`Result of MSEA(ORA)`[, 1])
+    Z <- rbind(Z, z)
   }
-
   return(Z)
 }
+
